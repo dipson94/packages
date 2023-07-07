09f187643f4b2235d9d29a698ee582e5d3a6f631
dipson94 <dipson94@gmail.com>
2023-07-07 22:36:57 +0200

    fixes

--- webscrape/setup.py
+++ webscrape/setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
-    name='webscrape-dipson94',
-    version='0.0.1',
-    author='Dipson',
-    description='A webscrape tool',
-    packages=['webscrape'],
-    install_requires=[
-        'bs4','requests'],
+    name="webscrape_article_scrape",
+    version="0.0.10",
+    description="An id generator that generated various types and lengths ids",
+    package_dir={"": "webscrape"},
+    packages=find_packages(where="webscrape"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Dipson",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['bs4','requests'],
+    },
+    python_requires=">=3.10",
 )
